0d2b2baed11995977dcfdd94f7f082c711b0dcb0
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2021-04-13 16:00:25 +0600

    client

--- client/control.sh
+++ client/control.sh
@@ -9,12 +9,16 @@ RAW_STATUS_JSON=$(curl --request GET -sL --url "${GET_STATUS_URL}")
 NEED_REBOOT=$(echo "${RAW_STATUS_JSON}" | jq '.reboot')
 NEED_RESTART=$(echo "${RAW_STATUS_JSON}" | jq '.restart')
 
+echo "restart: ${NEED_RESTART}, reboot: ${NEED_REBOOT}"
+
 if [[ ${NEED_REBOOT} = 'true' ]]
 then
+  echo "will reboot now";
   $REBOOT_CMD
 fi
 
 if [[ ${NEED_RESTART} = 'true' ]]
 then
+  echo "will restart now";
   $RESTART_CMD
 fi
